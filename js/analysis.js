4b245a2bfe187d48640eb371c24dc0a8faaee66a
Jan Kretschmann <jankretschmann1@gmail.com>
2021-03-02 14:55:56 -0600

    first commit

---
+++ js/analysis.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function initialize() {
+    resizeCanvas();
+    window.addEventListener("resize", resizeCanvas, false);
+  }
+  
+  function resizeCanvas() {
+    canvas.width = (3 * window.innerWidth) / 4;
+    canvas.height = window.innerHeight / 2;
+    canvas.setAttribute("align", "center");
+  }
+
+  initialize();
